be4346e6b0a37f30acc924edeee94d305aa53816
Peyman <peymanbaqerian@gmail.com>
2026-07-22 03:57:44 +0330

    Fix Hero import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar';
-import Hero from './components/hero';
+import Hero from './components/Hero';
 import About from './components/About';
 import Music from './components/Music';
 import Gallery from './components/Gallery';
